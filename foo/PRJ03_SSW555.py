39bc46bb757d736ddde3d6b22b172c7388268fbc
Ziyu Zhang <zcvszzy@gmail.com>
2019-02-10 22:10:07 -0500

    Update PRJ03_SSW555.py

--- foo/PRJ03_SSW555.py
+++ foo/PRJ03_SSW555.py
@@ -23,17 +23,30 @@ class Family():
 class Repository():
     def __init__(self, dir_path=os.getcwd()):
         self.People = dict()
-        self.Family = dict()
+        self.Familis = dict()
         self.working_path = dir_path
 
-    def input_fmality():
+    def input_fmality(self):
         path = os.path.getcwd()
         filename = input("Input file name: ")
-        for fam_dic in get
-        fam_dic = get_fam(path, filename)
-        hus_id = get_individual_id(fam_dic)
+        child_ids = list()
+        fam_lst = list(get_fam(path, filename))
+        for fam_dic in fam_lst:
+            hus = self.get_individual(fam_dic['hus_name'])
+            wife = self.get_individual(fam_dic['wife_name'])
+            for child_name in fam_dic['child_names']:
+                child_ids.append(self.get_individual_id(child_name))
+            new_family = Family(fam_dic['fam_ID'], fam_dic['mar_date'], fam_dic['div_date'], hus, wife, child_ids)
+            self.Familis.append(new_family)
 
+    def get_individual(self, name):
+        for individual in self.People.values():
+            if individual.name == name:
+                return individual
 
-    def get_individual_id(name):
-        for name in People.values().name:
-            return ID
+    def get_individual_id(self, name):
+        for individual in self.People.values():
+            if name == individual.name:
+                return individual.ID
+
+    def out_put
