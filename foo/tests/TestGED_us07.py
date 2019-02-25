cd6711fe02309ed0430444dd6f4ae003af907105
Ziyu Zhang <zcvszy@gmail.com>
2019-02-25 18:57:19 -0500

    Fix bugs in test06 and 07.

--- foo/tests/TestGED_us07.py
+++ foo/tests/TestGED_us07.py
@@ -58,11 +58,10 @@ class TestGedcom(unittest.TestCase):
                 """
                     To make sure every one has birthday and age.
                 """
-        with self.assertRaises(ValueError):
-            self.assertEqual(a.us07_age_less_150('@I2@'), True)
-            """
-                If the age is more than 150 then should raise a ValueError and print("The age is more than 150")
-            """
+        self.assertEqual(a.us07_age_less_150('@I2@'), True)
+        """
+            If the age is more than 150 then should raise a ValueError and print("The age is more than 150")
+        """
 
 if __name__ == "__main__":
     unittest.main(exit=False, verbosity=2)
