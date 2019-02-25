ff75dfd44af701b5c711edd19002e9171cb76090
Sherman77 <44691915+Sherman77@users.noreply.github.com>
2019-02-25 18:12:06 -0500

    Update TestGED_us06.py
    Updated dir_path for Repository

--- foo/tests/TestGED_us06.py
+++ foo/tests/TestGED_us06.py
@@ -11,13 +11,13 @@ from GED import Individual, Repository
 #from HW04_pli import store_divorce_date,store_death_date,test_compare_divrdate_ddate
 
 '''US06	Divorce before death'''
-
+a = Repository(filename = "Project01_Xiaomeng Xu.ged", dir_path = '/home/travis/build/SamNormcoreWayne/SSW555_TeamPrj/docs')
 class TestGED(unittest.TestCase):
     '''test store_divorce_date() ,store_death_date() and compare_divrdate_ddate()'''
 
     def test_store_divorce_date(self):
         '''test store_divorce_date()'''
-        a = Repository("Project01_Xiaomeng Xu.ged", os.path.join(os.getcwd(), 'docs'))
+        #a = Repository("Project01_Xiaomeng Xu.ged", os.path.join(os.getcwd(), 'docs'))
         self.assertEqual(a.store_divorce_date("@F3@"),"1980-05-05") # 幸福美满的家庭没有离婚的情况。。。
         self.assertIsNone(a.store_divorce_date("@F4@")) # indi_id exist but death date doesn't exist return None
         with self.assertRaises(ValueError): # divr date doesn't exist -->raise valueError
@@ -25,7 +25,7 @@ class TestGED(unittest.TestCase):
 
     def test_store_death_date(self):
         '''test store_death_date()'''
-        a = Repository("Project01_Xiaomeng Xu.ged", os.path.join(os.getcwd(), 'docs'))
+        #a = Repository("Project01_Xiaomeng Xu.ged", os.path.join(os.getcwd(), 'docs'))
         self.assertIsNone(a.store_death_date("@I4@")) # indi_id exist but death date doesn't exist return None
         self.assertEqual(a.store_death_date("@I2@"),"14 MAR 2001")
         with self.assertRaises(ValueError): # indi_id doesn't exist raise valueError
@@ -33,7 +33,7 @@ class TestGED(unittest.TestCase):
 
     def test_compare_divrdate_ddate(self):
         '''test compare_divrdate_ddate()'''
-        a = Repository("Project01_Xiaomeng Xu.ged", os.path.join(os.getcwd(), 'docs'))
+        #a = Repository("Project01_Xiaomeng Xu.ged", os.path.join(os.getcwd(), 'docs'))
         self.assertTrue(a.compare_divrdate_ddate("@F3@", "@I2@", "@I3@")) # death date is later than divorce 同样幸福美满没有离婚的
         with self.assertRaises(ValueError): # death date or divr date doesn't exist -->raise valueError
             a.compare_divrdate_ddate("@F4@", "@I1@", "@I4@")
