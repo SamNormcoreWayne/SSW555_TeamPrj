cc2663f1f0d2495a53ae83cb547dee01809eef16
Ziyu Zhang <zcvszy@gmail.com>
2019-02-25 20:52:49 -0500

    Add us01 and its test

--- foo/GED.py
+++ foo/GED.py
@@ -177,6 +177,10 @@ class Repository():
 
         print(table.get_string(sortby='ID'))
 
+    #us_01
+    def us_01_birth_b4_now(self):
+        for person in self.People.values():
+            yield person._bday
 
     #us_02
     def us02_birth_b4_marriage(self, fam_id):
