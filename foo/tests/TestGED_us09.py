18af67493a7d3622e3d360d379b147d28c740f37
JamesLi0217 <46985021+JamesLi0217@users.noreply.github.com>
2019-03-25 19:08:23 -0400

    Add files via upload

---
+++ foo/tests/TestGED_us09.py
@@ -0,0 +1,28 @@
+import unittest
+import datetime
+import sys
+import os
+from GED import Repository
+
+
+docs_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+a = Repository(filename='Project01_Xiaomeng Xu.ged', dir_path=os.path.join(docs_dir, 'docs'))
+
+class TestUS09(unittest.TestCase):
+    def test_us09_birth_b4_parents_death(self):
+        self.assertTrue(a.us09_birth_b4_parents_death('@I1@'))
+        self.assertTrue(a.us09_birth_b4_parents_death('@I2@'))
+        self.assertTrue(a.us09_birth_b4_parents_death('@I3@'))
+        self.assertTrue(a.us09_birth_b4_parents_death('@I4@'))
+        self.assertTrue(a.us09_birth_b4_parents_death('@I5@'))
+        self.assertTrue(a.us09_birth_b4_parents_death('@I6@'))
+        self.assertTrue(a.us09_birth_b4_parents_death('@I7@'))
+        with self.assertRaises(ValueError):
+            a.us09_birth_b4_parents_death('@I2@')
+            a.us09_birth_b4_parents_death('@I3@')
+            a.us09_birth_b4_parents_death('@I10@')
+
+
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main(exit = False, verbosity= 2)
