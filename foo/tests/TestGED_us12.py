b3c5f7bf20aa670f93a19b2e750f5cb502979b47
Ziyu Zhang <zcvszzy@gmail.com>
2019-03-25 22:49:51 -0400

    Fixed a bug that in us12 open a file that does not exist

--- foo/tests/TestGED_us12.py
+++ foo/tests/TestGED_us12.py
@@ -21,8 +21,6 @@ e = Repository(filename='Project_t04.ged',
                dir_path='/home/travis/build/SamNormcoreWayne/SSW555_TeamPrj/docs')
 f = Repository(filename='Project_t10.ged',
                dir_path='/home/travis/build/SamNormcoreWayne/SSW555_TeamPrj/docs')
-g = Repository(filename='Project_t16.ged',
-               dir_path='/home/travis/build/SamNormcoreWayne/SSW555_TeamPrj/docs')
 f = Repository(filename='Project01_us12_Pli.ged',
                dir_path='/home/travis/build/SamNormcoreWayne/SSW555_TeamPrj/docs')
 
@@ -34,7 +32,6 @@ class Test_us12_01(unittest.TestCase):
     'Project_t03.ged'
     'Project_t04.ged'
     'Project_t10.ged'
-    'Project_t16.ged'
     'Project01_us12_Pli.ged'
     """
 
@@ -46,7 +43,6 @@ class Test_us12_01(unittest.TestCase):
         self.assertTrue(d.us12_parents_not_2_old())
         self.assertTrue(e.us12_parents_not_2_old())
         self.assertTrue(f.us12_parents_not_2_old())
-        self.assertTrue(g.us12_parents_not_2_old())
         with self.assertRaises(TypeError):
             f.us12_parents_not_2_old()
 
