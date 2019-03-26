37b9d0c17147deda5e5129154e58b5c34b722ad3
JamesLi0217 <46985021+JamesLi0217@users.noreply.github.com>
2019-03-25 22:16:07 -0400

    Update GED.py

--- foo/GED.py
+++ foo/GED.py
@@ -388,6 +388,8 @@ class Repository():
                 for id in fam.child_id:
                     child_lst.append(self.getPeople(id))
                 break
+            else:
+                raise KeyError
         
         if len(child_lst) <= 1:
             """
