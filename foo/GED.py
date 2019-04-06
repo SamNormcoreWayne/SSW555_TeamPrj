f739b8120bad89506ab24f89591346d4db1c0097
JamesLi0217 <46985021+JamesLi0217@users.noreply.github.com>
2019-04-05 23:20:05 -0400

    US03,04,08,09

--- foo/GED.py
+++ foo/GED.py
@@ -21,7 +21,6 @@ from CheckGED import get_fam, get_indi
 from collections import defaultdict
 
 
-
 class Individual:
     """Repository where we store all information about each person"""
 
@@ -200,20 +199,21 @@ class Repository():
         """For a givenn fam_id, check the family marriage date and birthday for each individual, retuen the result"""
         for fam_id in self.Familis.keys():
             if self.Familis[fam_id].mar_date != 'NA':
-                
+
                 mdt = datetime.datetime.strptime(
                     self.Familis[fam_id].mar_date, '%Y-%m-%d')
                 for child_id in self.Familis[fam_id].child_id:
                     if self.People[child_id]._bday == 'N/A':
                         continue
                     else:
-                        bdt = datetime.datetime.strptime(self.People[child_id]._bday, '%d %b %Y')
+                        bdt = datetime.datetime.strptime(
+                            self.People[child_id]._bday, '%d %b %Y')
                         if bdt < mdt:
-                            print(f"ANOMALY: FAMILY:<{fam_id}>, US02: Child<{child_id}> born {bdt} before marriage on {mdt}")
-                
-
+                            print(
+                                f"ANOMALY: FAMILY:<{fam_id}>, US02: Child<{child_id}> born {bdt} before marriage on {mdt}")
 
     # us_03
+
     def find_indi_ddate(self, indi_id):
         ''' find individual death date by id in family tree'''
         for i in self.People.values():
@@ -224,8 +224,8 @@ class Repository():
                 else:
                     return None
                 # return i._dday if i._dday != "N/A" else None
-        else:
-            raise ValueError
+        '''else:
+            raise ValueError'''
 
     def find_indi_bdate(self, indi_id):
         ''' find individual birth date by id in family tree'''
@@ -236,48 +236,39 @@ class Repository():
         else:
             raise ValueError
 
-    def us03_birth_b4_death(self, indi_id):
+    def us03_birth_b4_death(self):
         ''' compare individual death date and married date by id in family tree'''
+        for indi_id in self.People:
+            birth_date = self.find_indi_bdate(indi_id)
 
-        birth_date = self.find_indi_bdate(indi_id)
+            death_date = self.find_indi_ddate(indi_id)
 
-        death_date = self.find_indi_ddate(indi_id)
-
-        if death_date == None:
-            raise ValueError(f"ID: {indi_id}, Death date not available")
-        elif birth_date == None:
-            raise ValueError(f"ID: {indi_id}, Birth date not available")
-        else:
-
-            #death_date_type = datetime.datetime.strptime(death_date, "%d %b %Y")
-            death_date = datetime.datetime.strptime(death_date, "%d %b %Y")
-            birth_date = datetime.datetime.strptime(birth_date, "%d %b %Y")
-
-        if death_date < birth_date:
-            return f"ID: {indi_id}, Result: Err: Birth before Death"
-        else:
-            return f"ID: {indi_id}, Result: Good"
+            if birth_date is None:
+                print(
+                    f"ANOMALY: US03: Individual{indi_id}> doesn't have birth date")
+            elif death_date:
+                death_date = datetime.datetime.strptime(death_date, "%d %b %Y")
+                birth_date = datetime.datetime.strptime(birth_date, "%d %b %Y")
+                if death_date < birth_date:
+                    print(
+                        f"ERROR: US03: Individual{indi_id}> birth date is after death date")
 
     # us_04
-    def us04_marriage_b4_divoce(self, fam_id):
+
+    def us04_marriage_b4_divoce(self):
         """Compare marriage date and divoce date(if available) for each family"""
 
-        #result = ''
-        mar_date = self.Familis[fam_id].mar_date
-        div_date = self.Familis[fam_id].div_date
-        if mar_date == 'NA':
-            result = "No marriage date"
-        elif div_date == 'NA':
-            result = "No divoce date"
-        else:
-            mar_dt = datetime.datetime.strptime(mar_date, "%Y-%m-%d")
-            div_dt = datetime.datetime.strptime(div_date, "%Y-%m-%d")
-            if mar_dt > div_dt:  # Check if marriage date comes after divoce date
-                result = "Err: Divoce before Marriage"
-            else:
-                result = "Good"
-
-        return f"ID: {fam_id}, Result: {result}"
+        for fam_id in self.Familis:
+            mar_date = self.Familis[fam_id].mar_date
+            div_date = self.Familis[fam_id].div_date
+            if mar_date == 'NA':
+                print(f"ANOMALY: US04: FAMILY:<{fam_id}> No marriage date")
+            elif div_date != 'NA':
+                mar_dt = datetime.datetime.strptime(mar_date, "%Y-%m-%d")
+                div_dt = datetime.datetime.strptime(div_date, "%Y-%m-%d")
+                if mar_dt > div_dt:  # Check if marriage date comes after divoce date
+                    print(
+                        f"ERROR: US04: FAMILY:<{fam_id}> Divoce before Marriage")
 
     # us_05
     def us05_marriage_b4_death(self):
@@ -300,10 +291,11 @@ class Repository():
                     if value != 'N/A':
                         ddt = datetime.datetime.strptime(value, '%d %b %Y')
                         if ddt < mdt:
-                            print(f"ERROR: FAMILY:<{fam_id}>, US05: Individule<{key}> die on {ddt} before marriage on {mdt}")
-
+                            print(
+                                f"ERROR: FAMILY:<{fam_id}>, US05: Individule<{key}> die on {ddt} before marriage on {mdt}")
 
     # us_06
+
     def store_divorce_date(self, id):
         the_date = str()
         for family in self.Familis.values():
@@ -386,28 +378,25 @@ class Repository():
                     # NA didn't record div date
                     return self.Familis[i._child].div_date if self.Familis[i._child].div_date != "NA" else None
 
-    def us08_birth_b4_parents_marriage(self, ind_id):
+    def us08_birth_b4_parents_marriage(self):
         '''Children should be born after marriage of parents (and not more than 9 months after their divorce)'''
-        try:
-            birth_date = self.find_indi_bdate(ind_id)
-            married_date = self.find_parents_mdate(ind_id)
-            divoce_date = self.find_parents_divdate(ind_id)
-        except ValueError:
-            return "Can't find!"
-        else:
-            if divoce_date != "Can't find!" and married_date != "Can't find!":
-                last_date = datetime.datetime.strptime(divoce_date, "%Y-%m-%d")+ datetime.timedelta(days=+270)
-                birth_date= datetime.datetime.strptime (birth_date, "%d %b %Y")
-                divoce_date= datetime.datetime.strptime (divoce_date, "%Y-%m-%d")
-                married_date= datetime.datetime.strptime (married_date, "%Y-%m-%d")
-                if birth_date > married_date and divoce_date is None:
-                    return True
-                elif birth_date > married_date and last_date >= birth_date:
-                    return True
-                else:
-                    return False
+
+        for indi_id in self.People:
+            birth_date = self.find_indi_bdate(indi_id)
+            married_date = self.find_parents_mdate(indi_id)
+            divoce_date = self.find_parents_divdate(indi_id)
+
+            birth_date = datetime.datetime.strptime(birth_date, "%d %b %Y")
+            if divoce_date == "Can't find!" or married_date == "Can't find!" or married_date is None:
+                print(f"ANOMALY: US08: Individual{indi_id}> Can't compare marriage date and divorce date")
             else:
-                return "Can't compare!"
+                married_date = datetime.datetime.strptime(married_date, "%Y-%m-%d")
+                if birth_date < married_date:
+                    print(f"ERROR: US08: Individual{indi_id}> Birth date is before Marriage")
+                if divoce_date:
+                    last_date = datetime.datetime.strptime(divoce_date, "%Y-%m-%d") + datetime.timedelta(days=+270)
+                    if last_date <= birth_date:
+                        print(f"ERROR: US08: Individual{indi_id}> Birth date is before Marriage")
 
     # us_09
     def find_mother_id(self, ind_id):
@@ -420,7 +409,8 @@ class Repository():
                     return "Can't find!"
                 else:
                     # 1)NA didn't record married date 2) I think wife_id(indi's mother) have to exist
-                    return self.Familis[i._child].wife_id #if self.Familis[i._child].wife_id != "NA" else None
+                    # if self.Familis[i._child].wife_id != "NA" else None
+                    return self.Familis[i._child].wife_id
 
     def find_father_id(self, ind_id):
 
@@ -432,8 +422,9 @@ class Repository():
                     return "Can't find!"
                 else:
                     # 1)NA didn't record married date 2) I think wife_id(indi's mother) have to exist
-                    return self.Familis[i._child].hus_id #if self.Familis[i._child].hus_id != "NA" else None
-    
+                    # if self.Familis[i._child].hus_id != "NA" else None
+                    return self.Familis[i._child].hus_id
+
     # us3 defined the function below
     '''def find_indi_ddate(self, indi_id):
         """ find individual death date by id in family tree"""
@@ -448,25 +439,27 @@ class Repository():
         else:
             raise ValueError'''
 
-    def us09_birth_b4_parents_death(self, ind_id):
+    def us09_birth_b4_parents_death(self):
         '''Child should be born before death of mother and before 9 months after death of father'''
-        birth_date = self.find_indi_bdate(ind_id)
-        mother_id = self.find_mother_id(ind_id)
-        mother_ddate = self.find_indi_ddate(mother_id)
-        father_id = self.find_father_id(ind_id)
-        father_ddate = self.find_indi_ddate(father_id)
-        try:
-            datetime.datetime.strptime(father_ddate, "%d %b %Y")
-            mother_ddate_dt = datetime.datetime.strptime(mother_ddate, "%d %b %Y")
-            birth_date_dt= datetime.datetime.strptime(birth_date, "%d %b %Y")
-        except TypeError:
-            return "Can't compare"
-        else:
-            last_date = datetime.datetime.strptime(father_ddate, "%d %b %Y") + datetime.timedelta(days=+270)
-            if birth_date_dt < last_date and birth_date_dt <= mother_ddate_dt:
-                return True
+        for ind_id in self.People:
+
+            birth_date = self.find_indi_bdate(ind_id)
+            mother_id = self.find_mother_id(ind_id)
+            mother_ddate = self.find_indi_ddate(mother_id)
+            father_id = self.find_father_id(ind_id)
+            father_ddate = self.find_indi_ddate(father_id)
+            try:
+                datetime.datetime.strptime(father_ddate, "%d %b %Y")
+                mother_ddate_dt = datetime.datetime.strptime(mother_ddate, "%d %b %Y")
+                birth_date_dt = datetime.datetime.strptime(birth_date, "%d %b %Y")
+            except TypeError:
+                print(f"ANOMALY: US09: Individual{ind_id}> Can't compare birth date and parents' death date")
+
             else:
-                return False
+                last_date = datetime.datetime.strptime(father_ddate, "%d %b %Y") + datetime.timedelta(days=+270)
+                if birth_date_dt > last_date or birth_date_dt > mother_ddate_dt:
+                
+                    print(f"ERROR: US09: Individual{ind_id}> Birth date is after parents' death date")
 
     # us_10
 
@@ -495,8 +488,8 @@ class Repository():
             else:
                 result = 'Good'
         return f"ID: {fam_id}, Result: {result}"
-    
-#us_12
+
+# us_12
     def us12_parents_not_2_old(self):
         """
             Fixed a bug that in for loop, it should read values instead of key-value pairs.
@@ -525,47 +518,54 @@ class Repository():
             if (childs is not None):
                 for child in childs:
                     if child._age == "N/A":
-                        raise TypeError("Child {id} age does not exist.".format(id=child._id))
+                        raise TypeError(
+                            "Child {id} age does not exist.".format(id=child._id))
 
                 for child in childs:
                     if ((wife._age - child._age) > 60) and (wife is not None):
-                        raise TypeError("Mother is too young or child {id} is too old!".format(id=child._id))
+                        raise TypeError(
+                            "Mother is too young or child {id} is too old!".format(id=child._id))
                     if ((hus._age - child._age) > 80) and (hus is not None):
-                        raise TypeError("Father is too young or child {id} is too old!".format(id=child._id))
+                        raise TypeError(
+                            "Father is too young or child {id} is too old!".format(id=child._id))
                 return True
             return "No Children"
 
-    #us_14
+    # us_14
     def us14_multiple_birth_less_5(self):
         """Given a fam_id, check for all child's birthday within the family, no more than 5 siblings should be born at the same time"""
         for fam_id, fam in self.Familis.items():
 
-            if len(fam.child_id) >= 5:  #Check for family with more than 5 children
+            if len(fam.child_id) >= 5:  # Check for family with more than 5 children
 
                 dd = defaultdict(int)
-                for i in fam.child_id:  #Initiate the defaultdict if there is nothing in dd.keys()
+                for i in fam.child_id:  # Initiate the defaultdict if there is nothing in dd.keys()
                     if dd.keys() == []:
                         dd[self.People[i]._bday] += 1
                     else:
-                        for date in dd.keys():  #If there is something in dd,keys(), compare the birthday of new child_id with all existing key in dd.keys()
-                            dt1 = datetime.datetime.strptime(self.People[i]._bday, '%d %b %Y')
+                        for date in dd.keys():  # If there is something in dd,keys(), compare the birthday of new child_id with all existing key in dd.keys()
+                            dt1 = datetime.datetime.strptime(
+                                self.People[i]._bday, '%d %b %Y')
                             dt2 = datetime.datetime.strptime(date, '%d %b %Y')
-                            days = abs(dt1 - dt2).days      #How many dates in between birthday of new child_id and one existing key in dd
-                            if 0 <= days <= 1:  #If within one date, add to existing key
+                            # How many dates in between birthday of new child_id and one existing key in dd
+                            days = abs(dt1 - dt2).days
+                            if 0 <= days <= 1:  # If within one date, add to existing key
                                 dd[date] += 1
                                 break
-                        
+
                         else:
-                            dd[self.People[i]._bday] += 1   #If more than one date, count as new key.
-                
+                            # If more than one date, count as new key.
+                            dd[self.People[i]._bday] += 1
+
                 for num in dd.values():
                     if num >= 5:
-                        print(f"Error: FAMILY<{fam_id}>, US14: Multiple birth more than 5!")
+                        print(
+                            f"Error: FAMILY<{fam_id}>, US14: Multiple birth more than 5!")
 
+    # us_11
 
-    #us_11
     def US11_No_Bigamy(self):
-      # For a given ind_id, check if the individual has more than 1 spounse during each marriage   
+      # For a given ind_id, check if the individual has more than 1 spounse during each marriage
         for fam_1 in self.Familis.values():
             for fam_2 in self.Familis.values():
                 if fam_1.fam_ID != fam_2.fam_ID:
@@ -576,8 +576,8 @@ class Repository():
                             return 'bigamy'
         else:
             return True
-        
-    #us_15
+
+    # us_15
     def US15_Fewer_15_Child(self):
       # For a given fam_id, check if the family has more than 15 children
 
@@ -587,27 +587,30 @@ class Repository():
                 break
         else:
             flag = True
-            
+
         return flag
 
+    # us_16
 
-    #us_16
     def us16_male_last_names(self):
         """Check within a family to see if husband's lastname matches with child's lastname"""
         for fam in self.Familis.values():
             hus_lastname = ''
             child_lastname = []
             if fam.hus_id != 'NA' and fam.child_id != ['NA']:
-                hus_lastname = (self.People[fam.hus_id]._name).rstrip('/').split('/').pop()
+                hus_lastname = (self.People[fam.hus_id]._name).rstrip(
+                    '/').split('/').pop()
                 for i in fam.child_id:
-                    child_lastname.append((self.People[i]._name).rstrip('/').split('/').pop())
+                    child_lastname.append(
+                        (self.People[i]._name).rstrip('/').split('/').pop())
 
                 for i in child_lastname:
                     if i != hus_lastname:
-                        print(f"Error: FAMILY:<{fam.fam_ID}>, US16: Last names don't match")
+                        print(
+                            f"Error: FAMILY:<{fam.fam_ID}>, US16: Last names don't match")
 
+    # us_13
 
-    #us_13
     def us13_sibling_spacing(self, fam_id):
         '''Birth dates of siblings should be more than 8 months apart or less than 2 days apart (twins may be born one day apart'''
         child_lst = list()
@@ -618,7 +621,7 @@ class Repository():
                 break
         else:
             raise KeyError
-        
+
         if len(child_lst) <= 1:
             """
             No siblings
@@ -635,20 +638,24 @@ class Repository():
                             raise TypeError("Wrong birthday between siblings")
             return True
 
+
 def main():
-    path = input("Input path: ")
-    filename = input("Input filename: ")
+    '''path = input("Input path: ")
+    filename = input("Input filename: ")'''
+    path = r"D:\sit study\SSW555\PJ"
+    filename = r"Project01_Xiaomeng Xu.ged"
     rep = Repository(filename=filename, dir_path=path)
     rep.individual_pt()
     rep.output_family()
     rep.us02_birth_b4_marriage()
+    rep.us03_birth_b4_death()
+    rep.us04_marriage_b4_divoce()
     rep.us05_marriage_b4_death()
+    rep.us08_birth_b4_parents_marriage()
+    rep.us09_birth_b4_parents_death()
     rep.us16_male_last_names()
     rep.us14_multiple_birth_less_5()
 
 
-
 if __name__ == "__main__":
     main()
-    
-
