fadf6f36c75531ef0bcf0ff2b82310d39c737170
JamesLi0217 <46985021+JamesLi0217@users.noreply.github.com>
2019-04-07 00:24:49 -0400

    test3

--- foo/tests/TestGED_us03.py
+++ foo/tests/TestGED_us03.py
@@ -28,16 +28,6 @@ class Test_us03_birth_b4_death(unittest.TestCase):
                            'ERROR: US03: Individual@I5@> birth date is after death date'], GED1.us03_birth_b4_death())
 
 
-class Test_us04_marriage_b4_divoce(unittest.TestCase):
-    """These are test cases for user story 04: Check if a family's marriage date is before divoce date(if available)"""
-    # Test cases for US_04
-
-    def test_GED2_family1(self):
-        """Testing function on file: Project_t04.ged"""
-        self.assertEqual(GED1.us04_marriage_b4_divoce(), ['ANOMALY: US04: FAMILY:<@F1@> No marriage date',
-                                                          'ERROR: US04: FAMILY:<@F2@> Divoce before Marriage', 'ERROR: US04: FAMILY:<@F3@> Divoce before Marriage'])
-
-
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main(exit=False, verbosity=2)
