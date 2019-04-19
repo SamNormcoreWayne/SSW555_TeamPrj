391de7b8071a240d7554a2744f1cad8f72b93f63
JamesLi0217 <46985021+JamesLi0217@users.noreply.github.com>
2019-04-18 23:58:19 -0400

    Update TestGED_us31.py

--- foo/tests/TestGED_us31.py
+++ foo/tests/TestGED_us31.py
@@ -15,8 +15,7 @@ class Test_us31_list_living_single(unittest.TestCase):
 
     def test_us31_list_living_single(self):
         """Test us31_list_living_single()"""
-        self.assertEqual(a.us31_list_living_single(), ['@I1@', '@I4@', '@I6@', '@I7@', '@I8@', '@I9@', '@I10@', '@I11@', '@I12@', '@I13@',
-                                                            '@I15@', '@I16@', '@I17@', '@I18@', '@I19@', '@I20@', '@I21@', '@I22@', '@I23@', '@I24@', '@I25@', '@I26@', '@I27@', '@I28@'])
+        self.assertEqual(a.us31_list_living_single(), [])
 
 
 if __name__ == '__main__':
