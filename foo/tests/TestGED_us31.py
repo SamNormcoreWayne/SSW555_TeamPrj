8703b342f5e18531db8742b20e77d5f795f8528e
JamesLi0217 <46985021+JamesLi0217@users.noreply.github.com>
2019-04-19 00:19:24 -0400

    Update TestGED_us31.py

--- foo/tests/TestGED_us31.py
+++ foo/tests/TestGED_us31.py
@@ -17,7 +17,16 @@ class Test_us31_list_living_single(unittest.TestCase):
         """Test us31_list_living_single()"""
         self.assertEqual(a.us31_list_living_single(), [])
 
+b = Repository(filename='Project_t31.ged',
+               dir_path=os.path.join(docs_dir, 'docs'))
 
+
+class Test_us31_list_living_single(unittest.TestCase):
+    """Tests conducted on file 'Project_t31.ged'"""
+
+    def test_us31_list_living_single(self):
+        """Test us31_list_living_single()"""
+        self.assertEqual(b.us31_list_living_single(), ['@I10@', '@I16@'])
 if __name__ == '__main__':
     print('Running unit tests')
     unittest.main(exit=False, verbosity=2)
